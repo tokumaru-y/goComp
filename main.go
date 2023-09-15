048819d5cc95d47f4412293e6c3cdb9f11ac2c46
tokumaru-y <tokumaru.program.yuuki@gmail.com>
2023-09-15 15:29:06 +0900

    add temp

--- main.go
+++ main.go
@@ -2,34 +2,30 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"math"
 	"os"
 	"strconv"
-	"strings"
 )
 
 var s = bufio.NewScanner(os.Stdin)
 
+func convInt(st string) int {
+	res, e := strconv.Atoi(st)
+	if e != nil {
+		panic("fail")
+	}
+	return res
+}
+
+func convIntFromStrings(st []string) []int {
+	var res []int
+	for _, s := range st {
+		res = append(res, convInt(s))
+	}
+
+	return res
+}
+
 func main() {
 	s.Scan()
 
-	ans := math.MaxInt64
-	s.Scan()
-	for _, a := range strings.Split(s.Text(), " ") {
-		num, _ := strconv.Atoi(a)
-		tmp := 0
-		for num%2 == 0 && num > 0 {
-			tmp++
-			num /= 2
-		}
-		if ans > tmp {
-			ans = tmp
-		}
-	}
-
-	if ans == math.MaxInt64 {
-		ans = 0
-	}
-	fmt.Println(ans)
 }
