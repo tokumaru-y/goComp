12f1689b53816fb1927abd8cdc7c89f79e890913
tokumaru-y <tokumaru.program.yuuki@gmail.com>
2023-09-17 11:27:16 +0900

    done 15

---
+++ _sumitted/guideline light bule/15.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var s = bufio.NewScanner(os.Stdin)
+
+func convInt(s string) int {
+	res, e := strconv.Atoi(s)
+	if e != nil {
+		panic("convInt failed")
+	}
+	return res
+}
+
+func getIntInput() int {
+	s.Scan()
+	st := s.Text()
+	res := convInt(st)
+
+	return res
+}
+
+func getIntInputFromOneLine() []int {
+	s.Scan()
+	st := strings.Split(s.Text(), " ")
+	var res []int
+	for _, s := range st {
+		i := convInt(s)
+		res = append(res, i)
+	}
+
+	return res
+}
+
+func reveseString(s string) string {
+	res := []rune(s)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		res[i], res[j] = res[j], res[i]
+	}
+
+	return string(res)
+}
+
+func getSubIntAbs(a, b int) int {
+	if a < b {
+		return b - a
+	} else {
+		return a - b
+	}
+}
+
+func Permute(nums []int) [][]int {
+	n := factorial(len(nums))
+	ret := make([][]int, 0, n)
+	permute(nums, &ret)
+	return ret
+}
+
+func permute(nums []int, ret *[][]int) {
+	*ret = append(*ret, makeCopy(nums))
+
+	n := len(nums)
+	p := make([]int, n+1)
+	for i := 0; i < n+1; i++ {
+		p[i] = i
+	}
+	for i := 1; i < n; {
+		p[i]--
+		j := 0
+		if i%2 == 1 {
+			j = p[i]
+		}
+
+		nums[i], nums[j] = nums[j], nums[i]
+		*ret = append(*ret, makeCopy(nums))
+		for i = 1; p[i] == 0; i++ {
+			p[i] = i
+		}
+	}
+}
+
+func factorial(n int) int {
+	ret := 1
+	for i := 2; i <= n; i++ {
+		ret *= i
+	}
+	return ret
+}
+
+func makeCopy(nums []int) []int {
+	return append([]int{}, nums...)
+}
+
+func main() {
+	N := getIntInput()
+	XY := [][]int{}
+	for i := 0; i < N; i++ {
+		t := getIntInputFromOneLine()
+		XY = append(XY, t)
+	}
+
+	var ans float64
+	var cnt float64
+	P := []int{}
+	for i := 0; i < N; i++ {
+		P = append(P, i)
+	}
+	for _, p := range Permute(P) {
+		for i := 0; i < (N - 1); i++ {
+			a, b := XY[p[i]][0], XY[p[i]][1]
+			c, d := XY[p[i+1]][0], XY[p[i+1]][1]
+			dis := float64((a-c)*(a-c) + (b-d)*(b-d))
+			ans += math.Sqrt(dis)
+		}
+		cnt++
+	}
+
+	fmt.Println(ans / cnt)
+}
